1461f1a55bd8e15e17095598fe78700a52a891dc
Laxe4k <Laxe4k@hotmail.com>
2025-06-03 09:48:35 +0200

    Enhance styling and responsiveness by updating CSS variables and improving layout for dark mode support; update font links in head.php to include Roboto.

--- assets/php/head.php
+++ assets/php/head.php
@@ -52,7 +52,7 @@ $lang = in_array($lang, ['fr', 'en']) ? $lang : 'en';
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 
     <title><?= htmlspecialchars($title) ?></title>
 </head>
